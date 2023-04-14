29b8ecbeb4b15a2465f8d247894eb86caa2febdc
Zach <zheylmun@gmail.com>
2023-04-13 21:52:25 -0400

    Improve subscription list ergonomics

--- src/market_data/mod.rs
+++ src/market_data/mod.rs
@@ -1,10 +1,8 @@
 use serde::{Deserialize, Serialize};
 pub mod stock_pricing;
 
-#[derive(Debug, Deserialize, Serialize)]
-
 /// All the possible market data types that can be subscribed to for streaming clients
-///
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct SubscriptionList {
     /// List of symbols for bars subscriptions
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -32,39 +30,43 @@ impl SubscriptionList {
     }
 
     /// Add a symbol to the bars subscription list
-    pub fn add_bars(&mut self, symbol: &str) {
+    pub fn add_bars(mut self, symbol: &str) -> Self {
         if let Some(bars) = &mut self.bars {
             bars.push(symbol.to_string());
         } else {
             self.bars = Some(vec![symbol.to_string()]);
         }
+        self
     }
 
     /// Add a symbol to the quotes subscription list
-    pub fn add_quotes(&mut self, symbol: &str) {
+    pub fn add_quotes(mut self, symbol: &str) -> Self {
         if let Some(quotes) = &mut self.quotes {
             quotes.push(symbol.to_string());
         } else {
             self.quotes = Some(vec![symbol.to_string()]);
         }
+        self
     }
 
     /// Add a symbol to the trades subscription list
-    pub fn add_trades(&mut self, symbol: &str) {
+    pub fn add_trades(mut self, symbol: &str) -> Self {
         if let Some(trades) = &mut self.trades {
             trades.push(symbol.to_string());
         } else {
             self.trades = Some(vec![symbol.to_string()]);
         }
+        self
     }
 
     /// Add a symbol to the news subscription list
-    pub fn add_news(&mut self, symbol: &str) {
+    pub fn add_news(mut self, symbol: &str) -> Self {
         if let Some(news) = &mut self.news {
             news.push(symbol.to_string());
         } else {
             self.news = Some(vec![symbol.to_string()]);
         }
+        self
     }
 }
 
